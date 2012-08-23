74f0ac81ec3fa1064f5bac2abb87d9d4d7f62a4c
ramdass <ramdassk@hotmail.com>
2012-08-23 15:36:50 +0530

    first

---
+++ src/GitJavaTest.java
@@ -0,0 +1,17 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: ramdass
+ * Date: Jul 16, 2012
+ * Time: 3:38:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GitJavaTest {
+    public static void main(String [] argv)
+    {
+            (new GitJavaTest()).printMessage();
+    }
+    public void printMessage()
+    {
+        System.out.println (" b-5:Changed on the branch1.");
+    }
+}
